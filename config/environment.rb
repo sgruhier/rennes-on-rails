84c83fd90d12cfb2228fc4626158cea41dbc4fd5
sgruhier <sgruhier@mac.com>
2009-06-21 18:45:04 +0200

    setup cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rspec", :lib => false, :version => ">= 1.2.0" 
   config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+  config.gem 'webrat'
   config.gem 'cucumber'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
